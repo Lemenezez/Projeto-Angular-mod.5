feedfca43a2f0138b916213fc055fe6479b93f75
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-10 17:18:26 -0300

    feat: Adiciona task.service com métodos para CRUD e atualização da lista de tarefas

---
+++ TODOLIST/src/app/services/task.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Task } from '../models/task.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private apiUrl = 'https://crudcrud.com/api/30e1214658a54393ba535a757fc917a6';
+  private taskListSource = new BehaviorSubject<Task[]>([]);
+  taskList$ = this.taskListSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/task`);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(`${this.apiUrl}/task`, task);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/task/${task.id}`, task);
+  }
+
+  deleteTask(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/task/${id}`);
+  }
+
+  updateTaskList() {
+    this.getTasks().subscribe({
+      next: (tasks) => this.taskListSource.next(tasks),
+      error: (error) => console.error('Erro ao atualizar taskList', error),
+    });
+  }
+}
