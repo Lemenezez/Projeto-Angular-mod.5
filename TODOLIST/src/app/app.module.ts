dc38237e5e289bdc0701b72a3df96f5c7f211cb5
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-10 17:17:27 -0300

    Configuração do módulo principal: registra a localidade pt-BR e importa os módulos essenciais.

--- TODOLIST/src/app/app.module.ts
+++ TODOLIST/src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskComponent } from './components/task/task.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalFormComponent } from './components/modal-form/modal-form.component';
+import { ListComponent } from './components/list/list.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskComponent,
+    ModalFormComponent,
+    ListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
