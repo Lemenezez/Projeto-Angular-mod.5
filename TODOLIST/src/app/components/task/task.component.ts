5da3f8b755a8a54ac2d6e06806dab9534e31be8c
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-10 17:21:45 -0300

    feat: TaskComponent

---
+++ TODOLIST/src/app/components/task/task.component.ts
@@ -0,0 +1,57 @@
+import { Component, ElementRef, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css'],
+})
+export class TaskComponent {
+  @Input() task!: Task;
+
+  inputCheck: FormGroup = new FormGroup({
+    checked: new FormControl(false),
+  });
+
+  constructor(private taskService: TaskService) {}
+
+  updateTask(task: Task): void {
+    this.taskService.updateTask(task).subscribe({
+      next: (response) => {
+        // logica para editar uma tarefa aqui
+
+        console.log('Task editada', response);
+      },
+      error: (error) => {
+        console.error('Erro ao editar task: ', error);
+      },
+      complete: () => {
+        console.log('Task editada com sucesso!');
+      },
+    });
+    console.log(task);
+  }
+
+  deleteTask(id: string): void {
+    this.taskService.deleteTask(id).subscribe({
+      next: (response) => {
+        // logica para deletar a tarefa aqui
+
+        console.log('Task removida', response);
+      },
+      error: (error) => {
+        console.error('Erro ao deletar task: ', error);
+      },
+      complete: () => {
+        console.log('Task deletada com sucesso!');
+      },
+    });
+  }
+
+  toggleTaskStatus(task: Task) {
+    task.checked = !task.checked;
+    console.log(task);
+  }
+}
